728774b8551f10c269ba8071af21877574d8d101
hayashi-3 <hayashi.gml@gmail.com>
2020-08-05 16:22:54 +0900

    [Finish]2-9

--- ruby確認問題/chapter9.rb
+++ ruby確認問題/chapter9.rb
@@ -1,5 +1,32 @@
-def hello_world
-  'Hello, World!'
+def greeting(name)
+	"Hello, #{name}!"
 end
 
-puts hello_world
+puts greeting('John')
+
+def greeting
+	'Hello, John!'
+	'Hi, John!'
+end
+
+puts greeting
+
+def greeting
+	return 'Hello, John!'
+	'Hi, John!'
+end
+
+puts greeting
+
+puts 10 + '1'.to_i
+
+puts 10.to_s + '1'
+
+puts 1  ==  2
+
+puts 1  ==  1
+
+puts 8 % 3
+
+puts 5 % 5
+
